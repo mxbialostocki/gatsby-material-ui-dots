a97f6dee148cb0f322ae2feea0a3ac5d6aadf603
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-27 11:31:38 +1200

    adjust sitetitle

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `dots`,
+    description: `A modified Gatsby starter incorporating MAterial-UI and some cool tricks for cool kids.`,
     author: `@gatsbyjs`,
   },
   plugins: [
