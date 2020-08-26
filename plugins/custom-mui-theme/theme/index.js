fb10224201db7d141cd120aaeb14218693a88178
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-26 12:11:08 +1200

    include custom overrides example

--- plugins/custom-mui-theme/theme/index.js
+++ plugins/custom-mui-theme/theme/index.js
@@ -1,10 +1,21 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import typography from './typography'
 
+// Set pallete primary and secondary colours here
 const palette = {
   primary: { main: '#f50057' },
   secondary: { main: '#D81B60' }
 };
+
+// set core overrides here using the Mui element
+const overrides = {
+  // this example MuiButton (https://material-ui.com/api/button/)
+  MuiButton: {
+    text: {
+      fontFamily: "'Roboto Mono', monospace, sans-serif"
+    }
+  }
+}
 
 const themeName = 'Dots Pew Invisibles Rosella';
 
